7746104b271f2b50405aebe2c88a9b46b2b4b821
Ben Sherlock <benclarence08@gmail.com>
2025-04-28 11:43:38 -0400

    Changed to sprite group, improved update function

--- track.py
+++ track.py
@@ -1,48 +1,27 @@
 import pygame
 from utils import *
-from enemy import Enemy
 
-class Track:
+class Track(pygame.sprite.Group):
     def __init__(self, targets: list[pygame.math.Vector2]):
+        super().__init__()
         self.targets = targets
-        self.enemies = pygame.sprite.Group()
 
 
-    def add_enemy(self, enemy: Enemy) -> None:
-        """
-        Add an enemy to the enemies list.
-        
-        :param enemy: Enemy to be added.
-        :return: Returns nothing.
-        """
-
-        self.enemies.add(enemy)
-
-
-    def update_enemies(self) -> None:
+    def update(self) -> None:
         """
         Updates the position of all enemies and changes their direction if needed. Draws all enemies.
 
         :return: Returns nothing.
         """
         
-        for enemy in self.enemies:
+        
+        for enemy in self:
             
             if not enemy.target: # set the enemy's target to the first target on the list
-                enemy.target = self.targets[0]
-
+                enemy.target = self.targets[0].copy()
+            
             if enemy.target == self.targets[-1] or not enemy.target: # stop the enemy from receiving new targets 
                 enemy.update(None)
 
             else: # once the enemy reaches its target, assign it a new one
-                enemy.update(self.targets[self.targets.index(enemy.target) + 1])
-                    
-
-    def draw_enemies(self) -> None:
-        """
-        Draws all enemies on the screen.
-        
-        :return: Returns nothing.
-        """
-
-        self.enemies.draw(get_window())
+                enemy.update(self.targets[list(self.targets).index(enemy.target) + 1].copy())
