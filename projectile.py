b272a029322c5e0f03d0b88e0c2114a8d6d8173f
Ben Sherlock <benclarence08@gmail.com>
2025-04-28 11:42:03 -0400

    Added image, updated movement

--- projectile.py
+++ projectile.py
@@ -2,26 +2,35 @@ import pygame
 from math import hypot
 from enemy import Enemy
 class Projectile(pygame.sprite.Sprite):
-    def __init__(self,  target: Enemy, damage: int):
+    def __init__(self,  target: Enemy, damage: int, pos: pygame.Vector2):
         super().__init__()
 
-        self.surf = pygame.surface.Surface((5,5))
-        self.surf.fill((100,100,100))
-        self.rect = self.surf.get_rect()
+        self.pos = pos
+
+        self.image = pygame.surface.Surface((5, 5))
+        self.image.fill((255,100,100))
+        self.rect = self.image.get_rect()
+
+        self.rect.center = self.pos
+
         self.damage = damage
 
         self.target = target
-        self.target_pos = self.target.get_pos()
-    def move(self) -> None:
+        self.target_pos = pygame.Vector2(self.target.get_pos())
+
+
+    def update(self) -> None:
         """
         Moves the projectile towards the target following the hypotenuse of their 2-dimensional distances.
 
         :return: Returns nothing.
         """
 
+        self.target_pos = pygame.Vector2(self.target.pos)
+
         # Calculates a horizontal line distance between the projectile and the target.
-        dx = self.pos.x - self.target_pos.x
-        dy = self.pos.y - self.target_pos.y
+        dx = self.target_pos.x - self.pos.x
+        dy = self.target_pos.y - self.pos.y 
 
         # Calculates the hypotenuse of this triangle
         dist = hypot(dx, dy)
@@ -32,8 +41,10 @@ class Projectile(pygame.sprite.Sprite):
             dy /= dist
 
         # Moves along the hypotenuse
-        self.pos.x += dx * 5
-        self.pos.y += dy * 5
+        self.pos.x += dx * 10
+        self.pos.y += dy * 10
+
+        self.rect.center = self.pos
 
         # If within 5 of the target, dissapears
         if dist <= 5:
