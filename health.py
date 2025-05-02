03f59f4e93e1da80ab97e50aaff787ae653ced68
Ben Sherlock <benclarence08@gmail.com>
2025-05-02 07:29:57 -0400

    Added health.py

---
+++ health.py
@@ -0,0 +1,45 @@
+class Health:
+    def __init__(self, starting_hp: int):
+        self.hp = starting_hp
+        Health.instance = self
+
+
+    def __add__(self, other):
+        return self.hp + other
+
+    def __iadd__(self, other):
+        self.hp += other
+        return self
+
+
+    def __sub__(self, other):
+        return self.hp - other
+
+    def __isub__(self, other: float):
+        self.hp -= other
+        return self
+
+
+    def __set__(self, other):
+        self.hp = other
+
+
+    def __lt__(self, other: float):
+        return self.hp < other
+    
+    def __gt__(self, other: float):
+        return self.hp > other
+
+    def __le__(self, other: float):
+        return self.hp <= other
+    
+    def __ge__(self, other: float):
+        return self.hp >= other
+
+
+    def __int__(self):
+        return self.hp
+
+
+    def __str__(self) -> str:
+        return "Health: " + str(self.hp)
